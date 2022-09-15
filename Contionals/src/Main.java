e16a38d5b46bdcbbce3aec117a5644d6d0e82635
Can Burak <98693116+Canbrk44@users.noreply.github.com>
2022-09-15 18:41:36 +0300

    If Bloklarıyla Çalışmak

---
+++ Contionals/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        int sayi = 3;
+        if (sayi<20){
+            System.out.println("Sayı 20'den Küçüktür");
+        } else if (sayi==20) {
+            System.out.println("Sayı 20'ye Eşittir");
+        } else {
+            System.out.println("Sayı 20'den Küçük Değildir");
+        }
+    }
+}
