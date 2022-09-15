3d7f59d21a53dd66b738c67f881d6a5423697dac
Can Burak <98693116+Canbrk44@users.noreply.github.com>
2022-09-15 19:02:02 +0300

    For Döngüsü

---
+++ LoopDemo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        //For
+        for (int i = 1;i<=10;i++){ //i+=2 ile iyi 2'şerli olarak artırabiliriz
+            System.out.println(i);
+        }
+        System.out.println("Döngü Bitti");
+    }
+}
