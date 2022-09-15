2eb49eb9034aef49205ed9ac5c1cade452518f57
Can Burak <98693116+Canbrk44@users.noreply.github.com>
2022-09-15 18:16:11 +0300

    Variables

---
+++ Variables/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args){
+        int ogrenciSayisi = 12; // integer değişken ve değer ataması
+        String mesaj = "Öğrenci Sayısı : "; // String metinsel ifade ile değişken oluşturma
+        System.out.println(mesaj + ogrenciSayisi); // integer ve string ifadeyi birleştirme
+        System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+
+    }
+}
