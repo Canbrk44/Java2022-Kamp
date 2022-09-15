f74be970497643712ffbc2e59160ddc8fe48fafe
Can Burak <98693116+Canbrk44@users.noreply.github.com>
2022-09-15 18:32:31 +0300

    Temel Veri Tipleriyle Çalışmak

---
+++ DataTypes/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        int sayi = 12; // -2,147,483,648 ile 2,147,483,647 arasındadır
+        byte sayi1 = 127; // -128 ile 127 arasındadır
+        short sayi2 = 9323; // -32768 ile 32767 arasında sayı tutmaktadır
+        long sayi3 = 23233333; //-9,223,372,036,854,775,808 ile 9,223,372,036,854,775,807 arasındadır
+        float sayi4 = 3.145f; // 1,40239846 ile 3,40282347 arasındadır
+        double sayi5 = 32.12; // 4.9406564584124654 ile 1.7976931348623157 arasındadır
+        char karakter = 'A'; // Tekbir karakteri ifade eder
+        boolean dogruMu = false; // Mantıksal veri türüdür
+        
+
+    }
+}
